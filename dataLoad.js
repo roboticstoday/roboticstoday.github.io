bfaf69b9cd793c1ce9b11ab6ee9dd87d2144f470
rachelholladay <rachel.holladay@gmail.com>
2020-05-07 23:48:07 -0400

    Add speaker list, watch page, social links and about paragraph

--- dataLoad.js
+++ dataLoad.js
@@ -17,7 +17,7 @@ function peopleLoad() {
             var picLink = x[i].getElementsByTagName("picLink")[0].childNodes[0].nodeValue;
 
             txt = '<p><a href="' + website + '" target="_blank">'
-            txt += '<img src="graphics/people/'+ picLink + '" alt="' + name + '"></a>';
+            txt += '<img src="graphics/organizers/'+ picLink + '" alt="' + name + '"></a>';
             txt += '<br/><a href="' + website + '" target="_blank">';
             txt += name + '</a><br/> ' + affliation + '</p>';
 
@@ -42,15 +42,15 @@ function seminarLoad(timeStatus) {
     for (i = 0; i < x.length; i++) {
         var speaker = x[i].getElementsByTagName("speaker")[0].childNodes[0].nodeValue;
         var affliation = x[i].getElementsByTagName("affliation")[0].childNodes[0].nodeValue;
-        var title = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-        var abstract_para = x[i].getElementsByTagName("abstract")[0].childNodes[0].nodeValue;
-        var bio = x[i].getElementsByTagName("bio")[0].childNodes[0].nodeValue;
         var picLink = x[i].getElementsByTagName("graphic")[0].childNodes[0].nodeValue;
         var date = x[i].getElementsByTagName("date")[0].childNodes[0].nodeValue;
         var meetingDate = Date.parse(date);
 
         if (timeStatus == 'past' && (Date.now() > meetingDate)) {
             var videoLink = x[i].getElementsByTagName("video")[0].childNodes[0].nodeValue;
+            var title = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+            var abstract_para = x[i].getElementsByTagName("abstract")[0].childNodes[0].nodeValue;
+            var bio = x[i].getElementsByTagName("bio")[0].childNodes[0].nodeValue;
 
             txt = '<div class="seminar">';
             txt += '<iframe width="448" height="252"  src="' + videoLink + '" frameborder="0" allowfullscreen></iframe>';
@@ -66,11 +66,21 @@ function seminarLoad(timeStatus) {
 
         } if (timeStatus == 'upcoming' && (Date.now() < meetingDate)) {
 
-            txt = '<div class="seminar"><img src="graphics/people/' + picLink + '" alt="' + speaker + '" />';
-            txt += '<h3>' + title + '</h3>';
-            txt += '<h3>' + date + ': ' + speaker + ' (' + affliation + ')</h3><br/>'
-            txt += '<p><b>Abstract:</b> ' + abstract_para + '<br/><br/>';
-            txt += '<b>Biography:</b> ' + bio;
+            txt = '<div class="seminar"><img src="graphics/speakers/' + picLink + '" alt="' + speaker + '" />';
+           
+            if ( x[i].getElementsByTagName("title").length > 0 ) { 
+                var title = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+                txt += '<h3>' + title + '</h3>';
+            }
+            txt += '<h3>' + date + ': ' + speaker + ' (' + affliation + ')</h3><br/>';
+            if ( x[i].getElementsByTagName("abstract").length > 0 ) {
+                var abstract_para = x[i].getElementsByTagName("abstract")[0].childNodes[0].nodeValue;
+                txt += '<p><b>Abstract:</b> ' + abstract_para + '<br/><br/>';
+            }
+            if ( x[i].getElementsByTagName("bio").length > 0 ) {
+                var bio = x[i].getElementsByTagName("bio")[0].childNodes[0].nodeValue;
+                txt += '<b>Biography:</b> ' + bio;
+            }
             if ( x[i].getElementsByTagName("note").length > 0 ) {
                 note = x[i].getElementsByTagName("note")[0].childNodes[0].nodeValue;
                 txt += '<br/>' + note; }
